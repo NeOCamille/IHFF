2d26ee12d48db08e46d17168b6a698fc5dc622ad
Camille <camille.schopmeijer@gmail.com>
2015-11-25 07:33:40 -0800

    Models

---
+++ IHFF Websystem/IHFF Websystem/Models/locatie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IHFF_Websystem.Models
+{
+    public class Locatie
+    {
+        [Required]
+        public int locatieID { get; set; }
+
+        [Required]
+        public string locatieNaam { get; set; }
+
+        [Required]
+        public string locatieAdres { get; set; }
+
+        [Required]
+        public int maxAantalPlaatsen { get; set; }
+
+        [Required]
+        public bool foodFilm { get; set; }
+
+        public Locatie()
+        {
+
+        }
+
+        public Locatie(int locatieID, string locatieNaam, string locatieAdres, int maxAantalPlaatsen, bool foodFilm)
+        {
+            this.locatieID = locatieID;
+            this.locatieNaam = locatieNaam;
+            this.locatieAdres = locatieAdres;
+            this.maxAantalPlaatsen = maxAantalPlaatsen;
+            this.foodFilm = foodFilm;
+        }
+
+    }
+}
