2030e9ecd6ad57ed7a9eaf3d52c1803c40fc37a4
Camille <camille.schopmeijer@gmail.com>
2015-11-25 08:06:28 -0800

    shizzle

---
+++ IHFF Websystem/IHFF Websystem/Models/IHFFContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace IHFF_Websystem.Models
+{
+    public class IHFFContext
+    {
+    }
+}
