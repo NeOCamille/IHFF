50db19915fb493b65eaf67c868cf59bf21472595
matthijs550 <matthijs_bierman@live.nl>
2015-12-28 23:45:31 +0100

    Popup WORKS!

--- IHFF Websystem/IHFF Websystem/Controllers/ProgrammaController.cs
+++ IHFF Websystem/IHFF Websystem/Controllers/ProgrammaController.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IHFF_Websystem.Models;
 
 namespace IHFF_Websystem.Controllers
 {
     public class ProgrammaController : Controller
     {
-        //
-        // GET: /Programma/
+        // eventList met alle events voor programma?
+        //List<Evenement> events;
+        Popup popup = new Popup();
 
         public ActionResult Index()
         {
@@ -18,27 +20,33 @@ namespace IHFF_Websystem.Controllers
 
         public ActionResult Woensdag()
         {
-            return View();
+            return View(popup);
         }
 
         public ActionResult Donderdag()
         {
-            return View();
+            return View(popup);
         }
 
         public ActionResult Vrijdag()
         {
-            return View();
+            return View(popup);
         }
 
         public ActionResult Zaterdag()
         {
-            return View();
+            return View(popup);
         }
 
         public ActionResult Zondag()
         {
-            return View();
+            return View(popup);
+        }
+
+        public void Popup(int i)
+        {
+            //Switch huidig popup
+            //popup.evenement = (Evenement)events.Where(e => e.evenementID == i);
         }
 
     }
