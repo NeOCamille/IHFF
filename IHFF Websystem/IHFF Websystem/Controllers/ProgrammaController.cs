8b2ea0050a7788a13da995fe44800ca1bc1ffe2c
matthijs550 <matthijs_bierman@live.nl>
2016-01-14 14:18:57 +0100

    format

--- IHFF Websystem/IHFF Websystem/Controllers/ProgrammaController.cs
+++ IHFF Websystem/IHFF Websystem/Controllers/ProgrammaController.cs
@@ -46,6 +46,8 @@ namespace IHFF_Websystem.Controllers
         {
             //Switch huidig popup
             popup.evenement = repository.GetEvent(id);
+            popup.datum = popup.evenement.startTijd.ToString("{0:dd-MM-YYYY}");
+            popup.tijd = popup.evenement.startTijd.ToString("{0:HH:mm}");
             popup.locatieNaam = repository.GetLocatie(popup.evenement.locatieID).locatieNaam;
             return View(popup);
         }
