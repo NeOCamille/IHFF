e0d2898f5d992a972a587c31620b20515fab1ad3
matthijs550 <matthijs_bierman@live.nl>
2016-01-14 12:14:26 +0100

    da

--- IHFF Websystem/IHFF Websystem/Content/JavaScript01.js
+++ IHFF Websystem/IHFF Websystem/Content/JavaScript01.js
@@ -14,6 +14,7 @@ function LoadPopup(url, changeClass) {
     $.get(url + '?_=' + (new Date()).getTime())
     .success(function (data) {
         $(changeClass).html(data);
+        alert();
     });
 }
 
